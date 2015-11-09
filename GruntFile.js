c332ac275e9b55446036d2a72dc5582e8f33ffab
Graham Cox <graham@grahamcox.co.uk>
2015-11-09 13:11:33 +0000

    Dropped in Mocha

--- GruntFile.js
+++ GruntFile.js
@@ -6,10 +6,27 @@ module.exports = function(grunt) {
     require('time-grunt')(grunt);
 
     grunt.initConfig({
+        mocha_istanbul: {
+            options: {
+                mochaOptions: ['--growl']
+            },
+            test: {
+                src: 'src/test',
+                options: {
+                    require: [
+                        'src/test/setup'
+                    ],
+                    mask: '**/*-spec.js',
+                    coverageFolder: 'target/coverage',
+                    reportFormats: ['html', 'lcov', 'text', 'text-summary'],
+                    root: 'src'
+                }
+            }
+        }
     });
 
     grunt.registerTask('build', []);
-    grunt.registerTask('test', []);
+    grunt.registerTask('test', ['build', 'mocha_istanbul:test']);
 
     grunt.registerTask('default', ['build', 'test']);
 };
